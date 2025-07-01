43935cd29ea5a1694871dd09f45616fa7e2074a0
yashbhangale <yashbhangale9@gmail.com>
2025-07-01 17:18:28 +0530

    sdnbjk,gvfnASDMK<FVNgjkasdbilgvfnbSK

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import { getAllBlogPosts } from '@/lib/blog'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://yashbhangale.github.io'
   
